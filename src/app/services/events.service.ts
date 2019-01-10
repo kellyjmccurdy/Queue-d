750eee914056e2531b4da979bf86dc922801e1d3
Zachary Espinoza <zacharyespinoza1@gmail.com>
2019-01-10 09:43:51 -0500

    fixed api typo

--- src/app/services/events.service.ts
+++ src/app/services/events.service.ts
@@ -10,11 +10,11 @@ export class EventsService {
     constructor(private _http: HttpClient) { }
 
     getEvents() {
-      return this._http.get(`${ApiUrl}/Events`, { headers: this.getHeaders() });
+      return this._http.get(`${ApiUrl}/Event`, { headers: this.getHeaders() });
     }
 
     createEvent(event: Event) {
-      return this._http.post(`${ApiUrl}/Events`, event, { headers: this.getHeaders()});
+      return this._http.post(`${ApiUrl}/Event`, event, { headers: this.getHeaders()});
     }
 
     private getHeaders() {
