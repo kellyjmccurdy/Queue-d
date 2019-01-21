7f4042f4c7c14b9f911703ed70a724d0c13f9500
Zachary Espinoza <zacharyespinoza1@gmail.com>
2019-01-21 12:04:11 -0500

    updated environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export let Api_Url = '';
 
 switch (window.location.hostname){
-  case '':
-    Api_Url +='https://localhost:44311/api'
+  case 'https://queuedapp.herokuapp.com/':
+    Api_Url +='https://queuedapi.azurewebsites.net/api'
     break;
   default:
-  Api_Url += 'https://queuedapi.azurewebsites.net/api';
+  Api_Url += 'https://localhost:44311/api';
 
 }
 
